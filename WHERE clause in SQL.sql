dcae87a55515e2ae126abaae95ab3a445cf4a4fc
Oreskov Stojance <133867596+oreskovs@users.noreply.github.com>
2025-04-02 08:10:38 +0200

    Add files via upload

---
+++ WHERE clause in SQL.sql
@@ -0,0 +1,11 @@
+-- Retrieve customers with a score not equal to 0 
+SELECT * 
+FROM customers
+WHERE score != 0 
+
+--Retrieve customers from Germany 
+SELECT 
+	first_name,
+	country
+FROM customers
+WHERE country = 'Germany'
