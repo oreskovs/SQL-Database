f38328584abd6f1844fb2a1e6d62cbaff1c46037
Oreskov Stojance <133867596+oreskovs@users.noreply.github.com>
2025-04-02 08:19:14 +0200

    Add files via upload

---
+++ ORDER BY in SQL.sql
@@ -0,0 +1,12 @@
+--Retrieve all customers and sort the results by the highest score first. 
+
+SELECT *
+FROM customers
+ORDER BY score DESC 
+
+--Retrieve all customers and sort the result by the lowest score first.
+
+SELECT *
+FROM customers
+ORDER BY score ASC
+							
