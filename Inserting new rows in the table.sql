a8faf43e9698b9ac2a7022803aa5bbfce664f8bb
Oreskov Stojance <133867596+oreskovs@users.noreply.github.com>
2025-04-05 10:22:22 +0200

    Add files via upload

---
+++ Inserting new rows in the table.sql
@@ -0,0 +1,9 @@
+-- Insert new row into customers table 
+
+INSERT INTO customers (id, first_name, country, score)
+VALUES
+	(6, 'Stojance', 'Macedonia', NULL),
+	(7, 'Nikola', NULL, 100),
+	(8, 'Maki', 'Germany', 200)
+
+SELECT * FROM customers
