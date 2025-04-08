712da930edafe0b8fa28625b5ac413306e7ba34d
Oreskov Stojance <133867596+oreskovs@users.noreply.github.com>
2025-04-08 08:00:44 +0200

    Add files via upload

---
+++ Search operator in SQL.sql
@@ -0,0 +1,19 @@
+-- Find all customers whose first name starts with M
+
+SELECT * FROM customers
+WHERE first_name LIKE 'M%'
+
+-- Find all customers whose first name ends with n
+
+SELECT * FROM customers 
+WHERE first_name LIKE '%n'
+
+-- Find all customers whose first name contains r
+
+SELECT * FROM customers
+WHERE first_name LIKE '%r%'
+
+-- Find all customers whose first name has 'r' in the third position
+
+SELECT * FROM customers
+WHERE first_name LIKE '__r%'
