29bdaf9156c08cb7e54d2a7c282d6cc8940499eb
Oreskov Stojance <133867596+oreskovs@users.noreply.github.com>
2025-04-10 10:00:51 +0200

    Add files via upload

---
+++ Number functions in SQL.sql
@@ -0,0 +1,12 @@
+-- SQL Functions
+-- Number Function 
+
+-- ROUND 
+SELECT 3.516 AS original_num,
+ROUND(3.516, 2) AS round_2,
+ROUND(3.516, 1) AS round_1,
+ROUND(3.516, 0) AS round_
+
+-- ABS = Returns the absolute (positive) value of a number, removing any negative sign.
+SELECT -10,
+ABS(-10)
